db3c50da8f751857419b48326b268a076ecb2fa8
Arseniy Poroshin <login@axxeny.com>
2022-02-23 04:15:10 +0300

    rulatin: cli

---
+++ latin_russian/rulatin/__main__.py
@@ -0,0 +1,12 @@
+from convert import convert
+import click
+
+
+@click.command()
+@click.option('-m', '--method', prompt='Enter method, please', help='Transliteration method (see methods.yaml).')
+def main(method: str) -> None:
+    while True:
+        print(convert(input(), method))
+
+
+main()
